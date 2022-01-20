54a3774d7584e06395e2a14898c79b3ec5bbe181
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-20 11:47:18 +0200

    FEAT: basic pages for questions and results

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Questions from '../views/Questions.vue'
+import Results from '../views/Results.vue'
 
 const routes = [
   {
@@ -8,12 +10,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/questions',
+    name: 'Questions',
+    component: Questions
+  },
+  {
+    path: '/results',
+    name: 'Results',
+    component: Results
   }
 ]
 
