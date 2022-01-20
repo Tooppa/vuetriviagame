c4638275710ca16bfa77d8b1643dfe3b91382cdd
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-20 09:56:14 +0200

    FEAT: Router now works. Added test about page

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <StartScreen msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import StartScreen from '@/components/StartScreen.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    StartScreen
+  }
+}
+</script>
