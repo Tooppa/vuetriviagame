f2de342c6db27bf79ff037eb483d9af106dfc806
Mikko <mikko.ryynanen2@gmail.com>
2022-01-21 15:36:19 +0800

    FEAT: question fetching from QuestionElement

--- src/components/QuestionElement.vue
+++ src/components/QuestionElement.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="hello">
-    <h2>Question</h2>
+    <!-- <h1 v-if="questions.value.length > 0" >{{questions.value[0].question}}</h1> -->
   </div>
 </template>
 
-<script>
-export default {
-  name: "QuestionElement",
-};
+<script setup>
+import { onMounted, ref } from 'vue';
+
+import { fetchQuestions } from '@/endpoints/trivia/triviaApi';
+
+const questions = ref(await fetchQuestions(1));
+
+onMounted(async () => {
+  // questions.value = ;
+  console.log(questions.value[0]);
+});
 </script>
 
+<style>
+</style>
