a94d1fdd88ba732afd236c76c418048f80abe2ac
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-21 15:49:41 +0200

    CHORE: Basic styling for the buttons

--- src/components/QuestionElement.vue
+++ src/components/QuestionElement.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="hello">
     <h2>{{ data.question }}</h2>
-    <button
-      v-for="answer in answers"
-      :key="answer"
-      @click="handleClick(answer)"
-    >
-      {{ answer }}
-    </button>
+    <div id="buttons">
+      <button
+        v-for="answer in answers"
+        :key="answer"
+        @click="handleClick(answer)"
+      >
+        {{ answer }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -18,7 +20,11 @@ export default {
   data() {
     let allAnswers = this.data.incorrect_answers;
     // adds correct answer in the wrong answer array. in the future add this to the endpoint
-    allAnswers.splice(Math.floor(Math.random() * allAnswers.length), 0, this.data.correct_answer);
+    allAnswers.splice(
+      Math.floor(Math.random() * allAnswers.length),
+      0,
+      this.data.correct_answer
+    );
     return {
       answers: allAnswers,
       handleClick: (clicked) => {
@@ -33,5 +39,15 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+#buttons {
+  display: flex;
+  flex-direction: column;
+  margin-left: 30vw;
+  margin-right: 30vw;
+}
+button{
+  margin: 10px;
+  padding: 10px;
+}
 </style>
