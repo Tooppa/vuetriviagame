dc62cb532d215750837a370e5830b627dcee8d02
Mikko <mikko.ryynanen2@gmail.com>
2022-01-24 19:14:36 +0200

    FEAT: Added score to store
    CHORE: Also updated the formatting of the file

--- src/store.js
+++ src/store.js
@@ -2,66 +2,70 @@ import { createStore } from "vuex";
 import { fetchQuestions } from "@/endpoints/trivia/triviaApi";
 
 const store = createStore({
-    state: {
-        questionType: {
-            amount: 10,
-            category: '',
-            difficulty: ''
-        },
-        questions: [],
-        answers: [],
-        currentQuestion: {},
-        questionIndex: 0,
-        user: {
-            name: '',
-            highscore: ''
-        }
+  state: {
+    questionType: {
+      amount: 10,
+      category: "",
+      difficulty: "",
     },
-    mutations: {
-        setQuestions: (state, payload) => {
-            state.questions = { ...payload }
-        },
-        setCurrentQuestion: (state, payload) => {
-            state.currentQuestion = { ...payload }
-        },
-        setSettings: (state, payload) => {
-            state.questionType = { ...payload }
-        },
-        setUser: (state, payload) => {
-            state.user = { ...payload }
-        }
+    questions: [],
+    answers: [],
+    currentQuestion: {},
+    questionIndex: 0,
+    user: {
+      name: "",
+      highscore: "",
+      score: "",
     },
-    actions: {
-        async fetch({ state, commit }) {
-            //get the questions from the endpoint
-            const questions = await fetchQuestions(
-                state.questionType.amount,
-                state.questionType.category,
-                state.questionType.difficulty
-            );
-            commit('setQuestions', questions);
-            commit('setCurrentQuestion', questions[0]);
-        },
-        async nextQuestion({state, commit}){
-            state.questionIndex++;
-            const nextIndex = state.questionIndex;
-            commit('setCurrentQuestion', state.questions[nextIndex]);
-        }
+  },
+  mutations: {
+    setQuestions: (state, payload) => {
+      state.questions = { ...payload };
     },
-    getters: {
-        getQuestions: (state) => {
-            return state.questions
-        },
-        getCurrentQuestion: (state) => {
-            return state.currentQuestion
-        },
-        checkIfLastQuestion: (state) =>{
-            return state.questionIndex === state.questionType.amount -1
-        },
-        getUser: (state) => {
-            return state.user;
-        }
-    }
-})
+    setCurrentQuestion: (state, payload) => {
+      state.currentQuestion = { ...payload };
+    },
+    setSettings: (state, payload) => {
+      state.questionType = { ...payload };
+    },
+    setUser: (state, payload) => {
+      state.user = { ...payload };
+    },
+    setScore: (state, payload) => {
+      state.user.score += payload;
+    },
+  },
+  actions: {
+    async fetch({ state, commit }) {
+      //get the questions from the endpoint
+      const questions = await fetchQuestions(
+        state.questionType.amount,
+        state.questionType.category,
+        state.questionType.difficulty
+      );
+      commit("setQuestions", questions);
+      commit("setCurrentQuestion", questions[0]);
+    },
+    async nextQuestion({ state, commit }) {
+      state.questionIndex++;
+      const nextIndex = state.questionIndex;
+      commit("setCurrentQuestion", state.questions[nextIndex]);
+    },
+  },
+  getters: {
+    getQuestions: (state) => {
+      return state.questions;
+    },
+    getCurrentQuestion: (state) => {
+      return state.currentQuestion;
+    },
+    checkIfLastQuestion: (state) => {
+      return state.questionIndex === state.questionType.amount - 1;
+    },
+    getUser: (state) => {
+      return state.user;
+    },
+  },
+});
 
 export default store;
