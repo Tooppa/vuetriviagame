e05c8125c2ae93e3f94b2fe3d51f1e3627c620a5
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-24 10:58:40 +0200

    CHORE: question element is now vue 3

--- src/components/QuestionElement.vue
+++ src/components/QuestionElement.vue
@@ -3,7 +3,7 @@
     <h2>{{ data.question }}</h2>
     <div id="buttons">
       <button
-        v-for="a in answers"
+        v-for="a in allAnswers"
         :key="a.answer"
         :class="{ show: a.show }"
         @click="handleClick(a)"
@@ -14,34 +14,29 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "QuestionElement",
-  props: ["data", "showanswer"],
-  data() {
-    console.log(this.data);
-    let allAnswers = this.data.incorrect_answers.map((answer) => {
-      return {
-        answer: answer,
-        show: false,
-      };
-    });
-    // adds correct answer in the wrong answer array. in the future add this to the endpoint
-    allAnswers.splice(Math.floor(Math.random() * allAnswers.length), 0, {
-      answer: this.data.correct_answer,
-      show: this.showanswer ? true : false,
-    });
-    return {
-      answers: allAnswers,
-      handleClick: (clicked) => {
-        console.log(
-          clicked.answer == this.data.correct_answer
-            ? "You answered correctly"
-            : "You answered incorrectly"
-        );
-      },
-    };
-  },
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  data: Object,
+  showanswer: Boolean
+})
+let allAnswers = props.data.incorrect_answers.map((answer) => {
+  return {
+    answer: answer,
+    show: false,
+  };
+});
+// adds correct answer in the wrong answer array. in the future add this to the endpoint
+allAnswers.splice(Math.floor(Math.random() * allAnswers.length), 0, {
+  answer: props.data.correct_answer,
+  show: props.showanswer ? true : false,
+});
+const handleClick = (clicked) => {
+  console.log(
+    clicked.answer == props.data.correct_answer
+      ? "You answered correctly"
+      : "You answered incorrectly"
+  );
 };
 </script>
 
