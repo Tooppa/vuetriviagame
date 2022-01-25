47257cc6538b78a76927695ca31acf0168273c4a
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-25 13:41:01 +0200

    FIX: fixed errors happening becouse state didnt exist if openin something else than the homepage

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="questions">
-    <h1>This is a questions page</h1>
-    <QuestionElement :data="question" />
+    <h1>Questions</h1>
+    <QuestionElement v-if="question.value" :data="question" />
   </div>
 </template>
 
 <script setup>
 import QuestionElement from "@/components/QuestionElement.vue";
-import {useStore} from 'vuex'
-import {computed} from 'vue'
+import { useStore } from "vuex";
+import { computed } from "vue";
 
 const store = useStore();
 const question = computed(() => store.getters.getCurrentQuestion);
--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,16 +1,22 @@
 <template>
   <SaveScoreButton />
-  <div class="results">
-    <h1>This is the results page</h1>
-    <QuestionElement v-for="(data, index) in questions" :key="data.question" :data="data" :showanswer="true" :index="Number(index)"/>
+  <h1>This is the results page</h1>
+  <div class="results" v-if="questions.value">
+    <QuestionElement
+      v-for="(data, index) in questions"
+      :key="data.question"
+      :data="data"
+      :showanswer="true"
+      :index="Number(index)"
+    />
   </div>
 </template>
 
 
 <script setup>
 import QuestionElement from "@/components/QuestionElement.vue";
-import {useStore} from 'vuex'
-import {computed} from 'vue'
+import { useStore } from "vuex";
+import { computed } from "vue";
 import SaveScoreButton from "../components/SaveScoreButton.vue";
 
 const store = useStore();
